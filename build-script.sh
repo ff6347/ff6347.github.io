738d76b50258d6840d5bf1d1de914ec7809378a6
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-12-04 07:15:15 +0100

    update build script

--- build-script.sh
+++ build-script.sh
@@ -7,7 +7,7 @@ function main(){
   echo "pulling from remote with git"
   git pull origin master
   echo "building with jekyll"
-  env JEKYLL_ENV=production jekyll b
+  env JEKYLL_ENV=production bundle exec jekyll b
   echo "copying all the files to the document root"
   cp -a /home/fabian/jekyll/fabianmoronzirfas.github.io/_site/. /home/fabian/html
 }
