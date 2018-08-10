2d904db97a4d0927fbde272e96731cf64f3beb4a
fmoronzirfas <fabian.moron-zirfas@ixds.com>
2018-08-10 11:38:23 +0200

    fix(error on other pages):

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,8 +8,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let age = calculateAge(new Date('1979-03-06T10:10:00'));
   console.log('Hey. You are peaking into the console? Nice. But there is not a lot going on here. Just some minor JS fiddle. Yes it uses webpack but actually only for fun and experimentation');
   let ele = document.querySelector('span#calced-age');
-  // console.log(age);
-  if (ele !== undefined){
+  // console.log(ele);
+
+  if (ele !== null){
 
     let counter = 0;
     let interval = setInterval(()=>{
--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-/* global window document $ */
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log('ready but nothing to see here :P')
-});
-
