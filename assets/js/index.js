9e77abafaa66c25c90bcf7115498efca0293dafd
fmoronzirfas <fabian.moron-zirfas@ixds.com>
2018-08-10 11:29:10 +0200

    feat(generate age): the age in the text is now generated by js

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+function calculateAge(birthday) { // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  let age = calculateAge(new Date('1979-03-06T10:10:00'));
+  console.log('Hey. You are peaking into the console? Nice. But there is not a lot going on here. Just some minor JS fiddle. Yes it uses webpack but actually only for fun and experimentation');
+  let ele = document.querySelector('span#calced-age');
+  // console.log(age);
+  let counter = 0;
+  let interval = setInterval(()=>{
+    if (counter === age){
+      // ele.classList.remove('shake');
+      clearInterval(interval);
+    } else {
+      counter++;
+      ele.innerHTML = counter;
+    }
+  }, 23);
+  // ele.innerHTML = age;
+  // console.log(ele);
+});
