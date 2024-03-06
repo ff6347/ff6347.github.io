fa44adb513551bf9b7cc6a01d8f8d9ff6214059b
ff6347 <hash@ff6347.email>
2024-03-06 06:32:18 +0100

    Add some more contacts and say x meh

---
+++ src/components/icon-bluesky.tsx
@@ -0,0 +1,6 @@
+const IconBluesky = () => (
+	<svg class="icon-bluesky">
+		<use xlink: href="/assets/fonts/icons.svg#icon-bluesky"></use>
+	</svg>
+);
+export default IconBluesky;
---
+++ src/components/icon-mastodon.tsx
@@ -0,0 +1,6 @@
+const IconMastodon = () => (
+	<svg class="icon-mastodon">
+		<use xlink: href="/assets/fonts/icons.svg#icon-mastodon"></use>
+	</svg>
+);
+export default IconMastodon;
