1887576b8fa19934095abf93a5b1a64b23d06a2d
ff6347 <hash@ff6347.email>
2024-07-17 11:07:42 +0200

    chore: Use smartypants

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,7 @@ export default defineConfig({
 	markdown: {
 		// Can be 'shiki' (default), 'prism' or false to disable highlighting
 		syntaxHighlight: "prism",
+		smartypants: true,
+		gfm: true,
 	},
 });
