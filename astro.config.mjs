ef332a97e76d07fe36be39e502dcb5c8ff23f091
ff6347 <hash@ff6347.email>
2024-08-30 12:19:32 +0200

    ci(ssr): needs adapter for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,24 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import mdx from "@astrojs/mdx";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "hybrid",
-	integrations: [react(), mdx()],
-	site: "https://fabianmoronzirfas.me",
-	markdown: {
-		remarkPlugins: [
-			[
-				remarkToc,
-				{
-					heading: "Contents",
-					tight: true,
-					maxDepth: 3,
-					ordered: false,
-				},
-			],
-		],
-		// Can be 'shiki' (default), 'prism' or false to disable highlighting
-		syntaxHighlight: "prism",
-		smartypants: true,
-		gfm: true,
-	},
+  output: "hybrid",
+  integrations: [react(), mdx()],
+  site: "https://fabianmoronzirfas.me",
+  markdown: {
+    remarkPlugins: [[remarkToc, {
+      heading: "Contents",
+      tight: true,
+      maxDepth: 3,
+      ordered: false
+    }]],
+    // Can be 'shiki' (default), 'prism' or false to disable highlighting
+    syntaxHighlight: "prism",
+    smartypants: true,
+    gfm: true
+  },
+  adapter: netlify()
 });
