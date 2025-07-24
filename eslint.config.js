863897ecd32a7100510e2fd02d782d3ac38f9524
Fabian Morón Zirfas <hash@ff6347.email>
2025-07-24 17:13:23 +0200

    chore: Configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+// import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	{
+		// Note: there should be no other properties in this object
+		ignores: [
+			".astro",
+			"node_modules",
+			"dist",
+			".netlify",
+			".claude",
+			".vscode",
+			"package-lock.json",
+			"pnpm-lock.yaml",
+			"yarn.lock",
+			"bun.lockb",
+			"bun.lock",
+			"bun.lockb",
+			"bun.lockb",
+		],
+	},
+	// ...eslintPluginAstro.configs.recommended,
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+	},
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		languageOptions: { globals: globals.browser },
+	},
+	tseslint.configs.recommended,
+	// pluginReact.configs.flat.recommended,
+	{
+		files: ["**/*.json"],
+		plugins: { json },
+		language: "json/json",
+		extends: ["json/recommended"],
+	},
+	{
+		files: ["**/*.jsonc"],
+		plugins: { json },
+		language: "json/jsonc",
+		extends: ["json/recommended"],
+	},
+	{
+		files: ["**/*.json5"],
+		plugins: { json },
+		language: "json/json5",
+		extends: ["json/recommended"],
+	},
+	{
+		files: ["**/*.md"],
+		languageOptions: { frontmatter: "yaml" },
+		plugins: { markdown },
+		language: "markdown/gfm",
+		extends: ["markdown/recommended"],
+	},
+	{
+		files: ["**/*.css"],
+		plugins: { css },
+		language: "css/css",
+		extends: ["css/recommended"],
+	},
+]);
