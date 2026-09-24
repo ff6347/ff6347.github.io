15978899d01b95a6b40d7505fb5db37fa8b09dcd
ff6347 <hash@ff6347.email>
2026-09-24 08:56:48 +0200

    feat: Drop netlify moving else where

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,40 +10,35 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	integrations: [react(), mdx()],
-	site: "https://fabianmoronzirfas.me",
+  output: "static",
+  integrations: [react(), mdx()],
+  site: "https://fabianmoronzirfas.me",
 
-	markdown: {
-		remarkPlugins: [
-			[
-				remarkToc,
-				{
-					heading: "Contents",
-					tight: true,
-					maxDepth: 3,
-					ordered: false,
-				},
-			],
-		],
-		rehypePlugins: [rehypeFigure],
-		// Can be 'shiki' (default), 'prism' or false to disable highlighting
-		syntaxHighlight: "prism",
-		smartypants: true,
-		gfm: true,
-	},
-
-	adapter: netlify(),
-
-	vite: {
-		plugins: [tailwindcss()],
-		define: {
-			"process.env.NODE_ENV": JSON.stringify(
-				process.env.NODE_ENV || "development",
-			),
-		},
-		optimizeDeps: {
-			include: ["react", "react-dom", "date-fns", "@observablehq/plot"],
-		},
-	},
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          heading: "Contents",
+          tight: true,
+          maxDepth: 3,
+          ordered: false,
+        },
+      ],
+    ],
+    rehypePlugins: [rehypeFigure],
+    // Can be 'shiki' (default), 'prism' or false to disable highlighting
+    syntaxHighlight: "prism",
+    smartypants: true,
+    gfm: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+    },
+    optimizeDeps: {
+      include: ["react", "react-dom", "date-fns", "@observablehq/plot"],
+    },
+  },
 });
